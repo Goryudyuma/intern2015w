af4f81c479b8c0ee12f45dee5e9d6cdc7fcb9970
Goryudyuma <umaretekyoumade@gmail.com>
2015-11-08 06:02:15 +0900

    login.phpにコメント追加

--- src/Controller/login.php
+++ src/Controller/login.php
@@ -8,18 +8,27 @@ use Baguette\Response;
  * @copyright 2015 pixiv Inc.
  * @license   WTFPL
  */
+
+//login処理
 final class login
 {
     public function action(\Baguette\Application $app, \Teto\Routing\Action $action)
     {
+		//もしすでにlogin済みならメインページへ飛ばす
         if ($app->session->get('user_id', ['default' => false])) {
             return new Response\RedirectResponse('/');
         }
 
         // systemは特殊なユーザーなのでログインできない
+		//  にしたってこれはひどい。せめて!==とすべき。
         if (isset($_REQUEST['user'], $_REQUEST['password']) && $_REQUEST['user'] != 'system') {
             $user = trim($_REQUEST['user']);
             $pass = $_REQUEST['password'];
+
+		//改行コードを入れたくなくてこうしてるのかな・・・？
+		//まずインジェクションが怖いのでプレースホルダを使う
+		//文字列連結しまくるのは速度の上でもよろしくないだろう。一文に持っていく。
+		//passwordがhash化されてなさそう・・・？流出事故の際に被害を減らすためにもhash化するべし。減災！
             $query
                 = 'SELECT `users`.`id`, `users`.`slug`, `users`.`name` '
                 . 'FROM `users` '
@@ -30,6 +39,9 @@ final class login
             $stmt = db()->prepare($query);
             $stmt->execute();
 
+		//セッション情報にログインしているかを持たせる。
+		//セッションIDをユーザーIDとは別にするべき。じゃないと乗っ取れる。
+		//方法がわからないので、調べながらやる。
             if ($login = $stmt->fetch(\PDO::FETCH_ASSOC)) {
                 $app->session->set('user_id', $login['id']);
                 $app->session->set('user_slug', $login['slug']);
@@ -38,6 +50,9 @@ final class login
             }
         }
 
+		//もしユーザー名とパスワードの組がDB上になければ、userはそのまま、パスワードを空にして返す。
+		//nullではなく''にするべき？
+		//??が使える。
         return new Response\TwigResponse('login.tpl.html', [
             'user' => isset($_REQUEST['user']) ? $_REQUEST['user'] : null,
         ]);
