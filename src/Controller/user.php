0535dee1e8b121f7a9e676d4c671363ecd77b9f3
Goryudyuma <umaretekyoumade@gmail.com>
2015-11-10 04:28:29 +0900

    user.phpにコメント追加

--- src/Controller/user.php
+++ src/Controller/user.php
@@ -8,11 +8,15 @@ use Nyaan\Response;
  * @copyright 2015 pixiv Inc.
  * @license   WTFPL
  */
+
+//ユーザーページを取ってくる
 final class user
 {
     public function action(\Baguette\Application $app, \Teto\Routing\Action $action)
     {
+		//@を抜いている
         $name = ltrim($action->param['user'], '@');
+		//プレースホルダ！
         $query = "SELECT * FROM `users` WHERE `slug` = \"{$name}\"";
         $stmt = db()->prepare($query);
         $stmt->execute();
